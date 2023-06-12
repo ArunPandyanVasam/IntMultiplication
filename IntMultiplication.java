bbd0bae9f8969661f43f244fa390b3f5de96cd51
ArunPandyanVasam <you@example.com>
2023-06-12 18:49:31 -0400

    version1

---
+++ IntMultiplication.java
@@ -0,0 +1,10 @@
+package intMultiplication;
+
+public class IntMultiplication {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+
+        System.out.println("x+y: " + (x * y));
+    }
+}
